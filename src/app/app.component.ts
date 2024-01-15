903970ee2f0136904e9c65aa09839ddfeafcca3f
rahbu <113043422+rahbu@users.noreply.github.com>
2024-01-15 11:23:11 -0800

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,3 +7,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {}
+
+/*
+
+1. Create a 'ring' component with a button labeled 'Ring', a button labeled 'Lock', and text that shows either 'Locked' or 'Unlocked'.  When the 'Lock' button is pressed, the text should show 'Locked'.
+
+2. We want to listen for sequences of 'ring' button presses, compare this against a saved sequence, and then switch from 'Locked' to 'Unlocked' if there's a match.
+
+*/
